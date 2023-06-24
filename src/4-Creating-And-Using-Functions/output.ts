772089fef217616defd7e944b1fbe7debccd6cfb
ash_zac <asherzac2020@gmail.com>
2023-06-23 18:57:00 -0700

    sf

--- src/4-Creating-And-Using-Functions/output.ts
+++ src/4-Creating-And-Using-Functions/output.ts
@@ -1,16 +1,13 @@
-const prefix = '🐉 ';
-
-function displayProductInfo(id: number, name: string) {
-  console.log(`${prefix} typed parameters`);
-  console.log(`product id=${id} and name=${name}`);
-}
-
-function runTheLearningSamples() {
-  displayProductInfo(10, 'Pizza');
-}
-
-runTheLearningSamples();
-
 export default async function updateOutput(id: string) {
-  // TODO: Implement the updateOutput function
+  runTheLearningSamples();
+}
+
+const prefix = '🐉 ';
+function runTheLearningSamples() {
+  function displayProductInfo(id: number, name: string) {
+    console.log(`${prefix} typed parameters`);
+    console.log(`product id=${id} and name=${name}`);
+  }
+
+  displayProductInfo(10, 'Pizza');
 }
